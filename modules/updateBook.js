c3b2c5a7d454532ce4386f7297a4b6f8165d0b94
Brady <brady.davenport@pm.me>
2022-05-25 20:08:39 -0600

    removing 'options' from updatedBook per Sheyna

--- modules/updateBook.js
+++ modules/updateBook.js
@@ -2,7 +2,7 @@ const Book = require('../models/book.js')
 
 async function updateBook(req, res) {
     try {
-        const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, options={overwrite:true, returnDocument: 'after'})
+        const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {overwrite:true, returnDocument: 'after'})
         res.status(200).send(updatedBook)
     } catch (err) {
         console.log(err)
